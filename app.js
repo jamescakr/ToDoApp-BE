d4514aac991a6e29e49d3147594bf0e95f2f34ca
jamesjo <jamescakr@gmail.com>
2024-10-17 06:09:46 -0600

    1st assignment

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const cors = require("cors");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+// require("dotenv").config();
+const app = express();
+// const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
+app.use(bodyParser.json());
+// app.use(cors());
+app.use("/api", indexRouter);
+// const mongoURI = MONGODB_URI_PROD;
+const mongoURI = `mongodb://localhost:27017/todo-demo`;
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("mongoose connected");
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
+
+app.listen(4000, () => {
+  console.log("server is on 4000");
+});
