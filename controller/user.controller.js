5c7fd0de1622de2226fb5b454f3ff3a4277a8d5b
jamesjo <jamescakr@gmail.com>
2024-10-20 21:03:23 -0600

    login lecture 1-3

---
+++ controller/user.controller.js
@@ -0,0 +1,40 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw new Error("user is already exist");
+    }
+    const salt = bcrypt.genSaltSync(saltRound);
+    const hash = bcrypt.hashSync(password, salt);
+    const newUser = new User({ email, name, password: hash });
+    await newUser.save();
+    res.status(200).json({ status: "success" });
+  } catch (error) {
+    res.status(400).json({ status: "failed", error });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("The user ID or password you entered is incorrect");
+  } catch (error) {
+    res.status(400).json({ status: "failed", error });
+  }
+};
+
+module.exports = userController;
